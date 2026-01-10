c409e2dc7d9e380eefb364137df1f39f7ae73da1
Michael Dell <apfelbaum24@gmail.com>
2026-01-10 23:04:44 +0100

    Upgrade next js

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  eslint: {
-    // Warning: This allows production builds to successfully complete even if
-    // your project has ESLint errors.
-    ignoreDuringBuilds: true,
-  },
+  trailingSlash: true,
 }
 
 export default nextConfig
